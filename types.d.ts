e12f350aa25b855d7c34caa9e0f11c53334caed7
fernandobouchet <fernandobouchet@gmail.com>
2024-10-02 21:47:59 -0300

    feat: update 'types'

--- types.d.ts
+++ types.d.ts
@@ -31,3 +31,38 @@ export interface Episode {
   crew: unknown[];
   guest_stars: unknown[];
 }
+
+export interface Series {
+  adult: boolean;
+  backdrop_path: string;
+  created_by: unknown[];
+  episode_run_time: number[];
+  first_air_date: string;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string;
+  last_episode_to_air: unknown;
+  name: string;
+  next_episode_to_air: null;
+  networks: Network[];
+  number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: unknown[];
+  production_countries: unknown[];
+  seasons: Season[];
+  spoken_languages: unknown[];
+  status: string;
+  tagline: string;
+  type: string;
+  vote_average: number;
+  vote_count: number;
+}
