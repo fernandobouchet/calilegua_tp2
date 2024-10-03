700fa2e32c4e8ef0acae81eea5405aeb3865043d
fernandobouchet <fernandobouchet@gmail.com>
2024-10-02 21:48:40 -0300

    refactor: modify dynamic route

--- app/(pages)/temporadas/[id]/page.tsx
+++ app/(pages)/temporadas/[id]/page.tsx
@@ -2,14 +2,18 @@ import EpisodeCard from "@/components/episodeCard";
 import getSeasonsById from "@/lib/getSeasonsById";
 import { Season } from "@/types";
 
-const Page = async ({ params }: { params: { temporadaId: number } }) => {
-  const data: Season = await getSeasonsById(params.temporadaId.toString());
+interface Props {
+  params: {
+    id: number;
+  };
+}
+
+const Page = async ({ params }: Props) => {
+  const data: Season = await getSeasonsById(params.id.toString());
   return (
     <section className="container mx-auto px-4 py-8 xl:max-w-screen-xl">
       <div className="pt-4 pb-8 text-center">
-        <h1 className="text-3xl font-bold mb-6">
-          Temporada {params.temporadaId}
-        </h1>
+        <h1 className="text-3xl font-bold mb-6">Temporada {params.id}</h1>
         <p>{data.overview}</p>
       </div>
       <div className="flex flex-col items-stretch sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-4 ">
