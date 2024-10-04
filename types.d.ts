f0061836b4e985bd3d652639a5376c85436384dd
fernandobouchet <fernandobouchet@gmail.com>
2024-10-04 20:51:15 -0300

    feat: update types

--- types.d.ts
+++ types.d.ts
@@ -66,3 +66,23 @@ export interface Series {
   vote_average: number;
   vote_count: number;
 }
+
+export interface Cast {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+  roles: CastRole[];
+  total_episode_count: number;
+  order: number;
+}
+
+export interface CastRole {
+  credit_id: string;
+  character: string;
+  episode_count: number;
+}
