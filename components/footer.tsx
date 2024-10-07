58748b833c68fcba41ea732857c64f7da176707f
fernandobouchet <fernandobouchet@gmail.com>
2024-10-07 00:29:52 -0300

    feat: update 'footer'

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,17 +1,15 @@
 const Footer = () => {
   return (
-    <footer className="bg-background border-t border-gray-500 mt-40">
-      <div className="container mx-auto py-4">
-        <div className="flex justify-between items-center">
-          <div className="text-sm text-gray-500 mb-4 sm:mb-0">© 2024</div>
-          <a
-            href="https://github.com/fernandobouchet/calilegua_tp2"
-            target="_blank"
-            className="text-sm text-gray-500 hover:text-gray-700 transition-colors"
-          >
-            Github
-          </a>
-        </div>
+    <footer className="mx-auto p-2 bg-background border-t border-gray-500 mt-40">
+      <div className="flex justify-between items-center">
+        <span className="text-sm text-gray-500">© 2024</span>
+        <a
+          href="https://github.com/fernandobouchet/calilegua_tp2"
+          target="_blank"
+          className="text-sm text-gray-500 hover:text-gray-700 transition-colors"
+        >
+          Github
+        </a>
       </div>
     </footer>
   );
