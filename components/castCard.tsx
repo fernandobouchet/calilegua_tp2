c86cf083e0b6f36bbe54405593b1639e63dc1ca4
fernandobouchet <fernandobouchet@gmail.com>
2024-10-07 20:41:49 -0300

    feat: add a link to the 'TMDB' info for 'castCard'

--- components/castCard.tsx
+++ components/castCard.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardFooter } from "./ui/card";
 import { Cast } from "@/types";
 import ImageCastCardLoader from "./imageCastCardLoader";
+import Link from "next/link";
 
 interface Props {
   cast: Cast;
@@ -8,22 +9,27 @@ interface Props {
 
 const CastCard = ({ cast }: Props) => {
   return (
-    <Card className="overflow-hidden w-40 lg:w-auto">
-      <CardContent className="p-0">
-        <div className="relative rounded-full min-h-[240px] lg:min-h-[300px]">
-          <ImageCastCardLoader
-            imageUrl={`https://image.tmdb.org/t/p/w200${cast.profile_path}`}
-            imageAlt={cast.name}
-          />
-        </div>
-      </CardContent>
-      <CardFooter className="flex flex-col items-center p-4">
-        <p className="text-lg font-semibold text-center">{cast.name}</p>
-        <p className="text-sm text-gray-500 text-center">
-          {cast.roles[0].character}
-        </p>
-      </CardFooter>
-    </Card>
+    <Link
+      href={`https://www.themoviedb.org/person/${cast.id}?language=es-ES`}
+      target="_blank"
+    >
+      <Card className="overflow-hidden w-40 lg:w-auto">
+        <CardContent className="p-0">
+          <div className="relative rounded-full min-h-[240px] lg:min-h-[300px]">
+            <ImageCastCardLoader
+              imageUrl={`https://image.tmdb.org/t/p/w200${cast.profile_path}`}
+              imageAlt={cast.name}
+            />
+          </div>
+        </CardContent>
+        <CardFooter className="flex flex-col items-center p-4">
+          <p className="text-lg font-semibold text-center">{cast.name}</p>
+          <p className="text-sm text-gray-500 text-center">
+            {cast.roles[0].character}
+          </p>
+        </CardFooter>
+      </Card>
+    </Link>
   );
 };
 
